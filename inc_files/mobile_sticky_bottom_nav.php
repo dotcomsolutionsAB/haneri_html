a4978f2df779853a663cf905014b783dc38e0333
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-01-14 17:44:39 +0530

    s13

---
+++ inc_files/mobile_sticky_bottom_nav.php
@@ -0,0 +1,55 @@
+<div class="sticky-info">
+    <a href="#">
+        <i class="icon-home"></i>Home
+    </a>
+</div>
+<div class="sticky-info">
+    <a href="#" class="">
+        <i class="icon-bars"></i>Brands
+    </a>
+</div>
+<div class="sticky-info">
+    <a href="#" class="">
+        <i class="icon-user-2"></i>Account
+    </a>
+</div>
+<div class="sticky-info">
+    <a href="#" class="">
+        <i class="icon-user-1"></i>Support
+    </a>
+</div>
+<!-- <div class="sticky-info">
+    <a href="#" class="">
+        <i class="icon-shopping-cart position-relative">
+            <span class="cart-count badge-circle">3</span>
+        </i>Cart
+    </a>
+</div> -->
+
+<!-- <div class="sticky-info">
+    <a href="demo3.html">
+        <i class="icon-home"></i>Home
+    </a>
+</div>
+<div class="sticky-info">
+    <a href="demo3-shop.html" class="">
+        <i class="icon-bars"></i>Categories
+    </a>
+</div>
+<div class="sticky-info">
+    <a href="wishlist.html" class="">
+        <i class="icon-wishlist-2"></i>Wishlist
+    </a>
+</div>
+<div class="sticky-info">
+    <a href="login.html" class="">
+        <i class="icon-user-2"></i>Account
+    </a>
+</div>
+<div class="sticky-info">
+    <a href="cart.html" class="">
+        <i class="icon-shopping-cart position-relative">
+            <span class="cart-count badge-circle">3</span>
+        </i>Cart
+    </a>
+</div> -->
--- index.php
+++ index.php
@@ -108,35 +108,10 @@
 
     </div><!-- End .mobile-menu-container -->
 
-    <!-- <div class="sticky-navbar">
-        <div class="sticky-info">
-            <a href="demo3.html">
-                <i class="icon-home"></i>Home
-            </a>
-        </div>
-        <div class="sticky-info">
-            <a href="demo3-shop.html" class="">
-                <i class="icon-bars"></i>Categories
-            </a>
-        </div>
-        <div class="sticky-info">
-            <a href="wishlist.html" class="">
-                <i class="icon-wishlist-2"></i>Wishlist
-            </a>
-        </div>
-        <div class="sticky-info">
-            <a href="login.html" class="">
-                <i class="icon-user-2"></i>Account
-            </a>
-        </div>
-        <div class="sticky-info">
-            <a href="cart.html" class="">
-                <i class="icon-shopping-cart position-relative">
-                    <span class="cart-count badge-circle">3</span>
-                </i>Cart
-            </a>
-        </div>
-    </div> -->
+    <div class="sticky-navbar">
+        <!-- Mobile sitky bottom nav -->
+         <?php include("inc_files/mobile_sticky_bottom_nav.php"); ?>
+    </div>
 
     <!-- <div class="newsletter-popup mfp-hide bg-img" id="newsletter-popup-form"
         style="background: #f1f1f1 no-repeat center/cover url(assets/images/newsletter_popup_bg.jpg)">
