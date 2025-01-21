de4d96cd3a72b384091b089f32760519590598dd
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-01-21 12:41:49 +0530

    s47

--- about.php
+++ about.php
@@ -49,7 +49,6 @@
                 <div class="container">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="demo3.html">Home</a></li>
-                        <li class="breadcrumb-item"><a href="#">Pages</a></li>
                         <li class="breadcrumb-item active" aria-current="page">About Us</li>
                     </ol>
                 </div><!-- End .container -->
@@ -63,63 +62,96 @@
                 </div><!-- End .page-header -->
 
                 <div class="container">
-                    <h2 class="page-intro font-weight-normal">Who we are</h2>
-                    <div class="row row-bg">
-                        <div class="col-md-6">
-                            <p class="mb-2">Aliquam consectetur et lorem semper scelerisque. Proin est nibh, vestibulum
-                                vitae congue
-                                nec,
-                                tristique eu justo. Maecenas eu nunc lacinia, porta lorem vitae, viverra velit. Nulla
-                                dolor
-                                libero, rhoncus quis luctus eu, fermentum sed leo. Morbi ut risus porttitor odio sodales
-                                pulvinar. Sed gravida nulla sed sapien vulputate, eget malesuada justo egestas.
-                                Pellentesque
-                                sem mi, vulputate ac iaculis sit amet, sagittis feugiat dui. Proin non pellentesque leo.
-                                Vestibulum varius laoreet posuere. Etiam fringilla diam odio.</p>
+                    <section id="our-story">
+                        <!-- <h2 class="page-intro font-weight-normal">Who we are</h2> -->
+                        <h1 class="page-title font4">OUR Story</h1>
+                        <div class="row row-bg">
+                            <div class="col-md-12" id="vision">
+                                <p class="mb-2">Aliquam consectetur et lorem semper scelerisque. Proin est nibh, vestibulum
+                                    vitae congue
+                                    nec,
+                                    tristique eu justo. Maecenas eu nunc lacinia, porta lorem vitae, viverra velit. Nulla
+                                    dolor
+                                    libero, rhoncus quis luctus eu, fermentum sed leo. Morbi ut risus porttitor odio sodales
+                                    pulvinar. Sed gravida nulla sed sapien vulputate, eget malesuada justo egestas.
+                                    Pellentesque
+                                    sem mi, vulputate ac iaculis sit amet, sagittis feugiat dui. Proin non pellentesque leo.
+                                    Vestibulum varius laoreet posuere. Etiam fringilla diam odio.</p>
 
-                            <p>Nulla felis nibh, bibendum a leo ut, egestas ornare felis. Nam pretium mauris justo, eget
-                                commodo est fringilla vel. Proin condimentum, lacus sit amet finibus gravida, sapien
-                                ligula
-                                mattis leo, sit amet mattis leo lacus sit amet lectus. Nulla facilisi. Etiam porta
-                                iaculis
-                                velit id pulvinar. Sed dolor odio, eleifend eget aliquam vitae, efficitur vitae dolor.
-                                Integer ornare neque ac leo sollicitudin, at vestibulum ante scelerisque. Nullam ut elit
-                                sed
-                                lorem tempus feugiat in et lectus. Mauris interdum molestie placerat. Nullam dolor nunc,
-                                elementum et tincidunt id, vestibulum quis turpis. Integer imperdiet a eros a laoreet.
-                                Nam
-                                mattis vel ligula non imperdiet. Suspendisse potenti. Fusce purus sem, dapibus eu
-                                fermentum
-                                eget, aliquam vehicula ligula.</p>
-                        </div><!-- End .col-md-6 -->
+                                <p>Nulla felis nibh, bibendum a leo ut, egestas ornare felis. Nam pretium mauris justo, eget
+                                    commodo est fringilla vel. Proin condimentum, lacus sit amet finibus gravida, sapien
+                                    ligula
+                                    mattis leo, sit amet mattis leo lacus sit amet lectus. Nulla facilisi. Etiam porta
+                                    iaculis
+                                    velit id pulvinar. Sed dolor odio, eleifend eget aliquam vitae, efficitur vitae dolor.
+                                    Integer ornare neque ac leo sollicitudin, at vestibulum ante scelerisque. Nullam ut elit
+                                    sed
+                                    lorem tempus feugiat in et lectus. Mauris interdum molestie placerat. Nullam dolor nunc,
+                                    elementum et tincidunt id, vestibulum quis turpis. Integer imperdiet a eros a laoreet.
+                                    Nam
+                                    mattis vel ligula non imperdiet. Suspendisse potenti. Fusce purus sem, dapibus eu
+                                    fermentum
+                                    eget, aliquam vehicula ligula.</p>
+                            </div><!-- End .col-md-6 -->
 
-                        <div class="col-md-6">
-                            <p class="mb-2">Quisque congue dignissim dui sed luctus. Morbi nec mi vitae magna finibus
-                                ullamcorper.
-                                Etiam
-                                mattis blandit convallis. Suspendisse eu elementum leo. Vestibulum molestie nunc et
-                                efficitur egestas. Vivamus arcu lectus, laoreet vel consectetur bibendum, elementum non
-                                nunc. Proin porttitor velit odio, ac mattis quam tincidunt eget. Fusce semper nunc eget
-                                efficitur efficitur. Nam ullamcorper, enim id tincidunt feugiat, velit mauris fermentum
-                                nulla, at tempor eros turpis sit amet massa. Ut a semper lectus, sed hendrerit risus. In
-                                hac
-                                habitasse platea dictumst. Curabitur venenatis cursus posuere. Praesent turpis nisi,
-                                aliquam
-                                at facilisis non, sagittis vel urna. Donec diam lorem, feugiat vitae laoreet in,
-                                sagittis a
-                                lorem.</p>
+                            <div class="col-md-12" id="mission">
+                                <p class="mb-2">Quisque congue dignissim dui sed luctus. Morbi nec mi vitae magna finibus
+                                    ullamcorper.
+                                    Etiam
+                                    mattis blandit convallis. Suspendisse eu elementum leo. Vestibulum molestie nunc et
+                                    efficitur egestas. Vivamus arcu lectus, laoreet vel consectetur bibendum, elementum non
+                                    nunc. Proin porttitor velit odio, ac mattis quam tincidunt eget. Fusce semper nunc eget
+                                    efficitur efficitur. Nam ullamcorper, enim id tincidunt feugiat, velit mauris fermentum
+                                    nulla, at tempor eros turpis sit amet massa. Ut a semper lectus, sed hendrerit risus. In
+                                    hac
+                                    habitasse platea dictumst. Curabitur venenatis cursus posuere. Praesent turpis nisi,
+                                    aliquam
+                                    at facilisis non, sagittis vel urna. Donec diam lorem, feugiat vitae laoreet in,
+                                    sagittis a
+                                    lorem.</p>
 
-                            <p>Aliquam consectetur et lorem semper scelerisque. Proin est nibh, vestibulum vitae congue
-                                nec,
-                                tristique eu justo. Maecenas eu nunc lacinia, porta lorem vitae, viverra velit. Nulla
-                                dolor
-                                libero, rhoncus quis luctus eu, fermentum sed leo. Morbi ut risus porttitor odio sodales
-                                pulvinar. Sed gravida nulla sed sapien vulputate, eget malesuada justo egestas.
-                                Pellentesque
-                                sem mi, vulputate ac iaculis sit amet, sagittis feugiat dui. Proin non pellentesque leo.
-                                Vestibulum varius laoreet posuere. Etiam fringilla diam odio.</p>
-                        </div><!-- End .col-md-6 -->
-                    </div><!-- End .row -->
+                                <p>Aliquam consectetur et lorem semper scelerisque. Proin est nibh, vestibulum vitae congue
+                                    nec,
+                                    tristique eu justo. Maecenas eu nunc lacinia, porta lorem vitae, viverra velit. Nulla
+                                    dolor
+                                    libero, rhoncus quis luctus eu, fermentum sed leo. Morbi ut risus porttitor odio sodales
+                                    pulvinar. Sed gravida nulla sed sapien vulputate, eget malesuada justo egestas.
+                                    Pellentesque
+                                    sem mi, vulputate ac iaculis sit amet, sagittis feugiat dui. Proin non pellentesque leo.
+                                    Vestibulum varius laoreet posuere. Etiam fringilla diam odio.</p>
+                            </div><!-- End .col-md-6 -->
+
+                            <div class="col-md-12" id="values">
+                                <p class="mb-2">Quisque congue dignissim dui sed luctus. Morbi nec mi vitae magna finibus
+                                    ullamcorper.
+                                    Etiam
+                                    mattis blandit convallis. Suspendisse eu elementum leo. Vestibulum molestie nunc et
+                                    efficitur egestas. Vivamus arcu lectus, laoreet vel consectetur bibendum, elementum non
+                                    nunc. Proin porttitor velit odio, ac mattis quam tincidunt eget. Fusce semper nunc eget
+                                    efficitur efficitur. Nam ullamcorper, enim id tincidunt feugiat, velit mauris fermentum
+                                    nulla, at tempor eros turpis sit amet massa. Ut a semper lectus, sed hendrerit risus. In
+                                    hac
+                                    habitasse platea dictumst. Curabitur venenatis cursus posuere. Praesent turpis nisi,
+                                    aliquam
+                                    at facilisis non, sagittis vel urna. Donec diam lorem, feugiat vitae laoreet in,
+                                    sagittis a
+                                    lorem.</p>
+                                    <p>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum nisi laboriosam voluptatibus rem id sunt temporibus quos. Dolor corrupti, maxime ratione error officiis cupiditate itaque, quos vero ea perferendis recusandae non? Vel iure distinctio enim ad eaque provident dignissimos ut placeat beatae adipisci pariatur exercitationem unde fuga, nisi harum minus consequatur id fugit nulla autem. Reiciendis laudantium voluptate quisquam mollitia omnis veritatis ex impedit at error delectus laborum, tempora velit officiis vero consectetur facere? Dolorem, quasi quo. Dolorem iusto ipsum ab repellat vero beatae sunt, libero culpa reiciendis unde numquam earum incidunt animi officia nisi? Reprehenderit amet magnam laboriosam quam molestias, dolores architecto reiciendis doloremque repellat nulla perferendis modi fugiat vel libero recusandae et esse, provident non delectus! Temporibus culpa eum, veniam cum eos architecto exercitationem obcaecati magnam quaerat, laudantium necessitatibus tempora delectus sit eaque ut? Sint, accusantium sed sunt quos dicta incidunt voluptates autem, aliquam animi saepe natus reprehenderit alias voluptatibus ullam molestias illum eaque maxime quis? Veritatis iure possimus aliquam minima veniam corrupti? Saepe eaque, libero non eius ex eligendi sequi nesciunt quasi possimus. Velit quisquam qui reiciendis quos sunt commodi in, beatae repellat voluptatum. Suscipit excepturi possimus ea est fugit modi totam ratione aliquam quos sequi? Placeat!
+                                    </p>
+
+                                <p>Aliquam consectetur et lorem semper scelerisque. Proin est nibh, vestibulum vitae congue
+                                    nec,
+                                    tristique eu justo. Maecenas eu nunc lacinia, porta lorem vitae, viverra velit. Nulla
+                                    dolor
+                                    libero, rhoncus quis luctus eu, fermentum sed leo. Morbi ut risus porttitor odio sodales
+                                    pulvinar. Sed gravida nulla sed sapien vulputate, eget malesuada justo egestas.
+                                    Pellentesque
+                                    sem mi, vulputate ac iaculis sit amet, sagittis feugiat dui. Proin non pellentesque leo.
+                                    Vestibulum varius laoreet posuere. Etiam fringilla diam odio.</p>
+                            </div><!-- End .col-md-6 -->
+                        </div><!-- End .row -->
+                    </section>
                 </div><!-- End .container -->
 
                 <div class="history-section pb-3">
