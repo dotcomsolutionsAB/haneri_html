2491a3de79169c477f5120b035933c50fe19547c
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-01-24 16:24:04 +0530

    S203

--- inc_files/header.php
+++ inc_files/header.php
@@ -67,39 +67,39 @@
                         <div class="megamenu megamenu-fixed-width megamenu-3cols categoryy">
                             <div class="row">
                             <section class="categories hover">
-    <div class="category hov">
-        <div class="img">
-            <img src="images/Ceilimg Fan.png" alt="Ceiling Fan">
-        </div>
-        <div class="text">
-            <p>Ceiling Fan</p>
-        </div>
-    </div>
-    <div class="category">
-        <div class="img">
-            <img src="images/Table Wall Pedestals.png" alt="Table Wall Pedestals">
-        </div>                
-        <div class="text">
-            <p>Table Wall Pedestals</p>
-        </div>
-    </div>
-    <div class="category">
-        <div class="img">
-            <img src="images/Domestic Exhaust.png" alt="Domestic Exhaust">
-        </div>
-        <div class="text">
-            <p>Domestic Exhaust</p>
-        </div>
-    </div>
-    <div class="category">
-        <div class="img">
-            <img src="images/Personal.png" alt="Personal">
-        </div>
-        <div class="text">
-            <p>Personal</p>
-        </div>
-    </div>
-</section>
+                                <div class="category hov">
+                                    <div class="img">
+                                        <img src="images/Ceilimg Fan.png" alt="Ceiling Fan">
+                                    </div>
+                                    <div class="text">
+                                        <p>Ceiling Fan</p>
+                                    </div>
+                                </div>
+                                <div class="category hov">
+                                    <div class="img">
+                                        <img src="images/Table Wall Pedestals.png" alt="Table Wall Pedestals">
+                                    </div>                
+                                    <div class="text">
+                                        <p>Table Wall Pedestals</p>
+                                    </div>
+                                </div>
+                                <div class="category hov">
+                                    <div class="img">
+                                        <img src="images/Domestic Exhaust.png" alt="Domestic Exhaust">
+                                    </div>
+                                    <div class="text">
+                                        <p>Domestic Exhaust</p>
+                                    </div>
+                                </div>
+                                <div class="category hov">
+                                    <div class="img">
+                                        <img src="images/Personal.png" alt="Personal">
+                                    </div>
+                                    <div class="text">
+                                        <p>Personal</p>
+                                    </div>
+                                </div>
+                            </section>
                             </div>
                         </div>
                     </li>
