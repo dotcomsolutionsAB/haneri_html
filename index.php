f54e28716cf01c928a2b1ed7fcef497ab0f581dd
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-01-25 11:42:45 +0530

    s234

--- index.php
+++ index.php
@@ -113,20 +113,22 @@
             <!-- support_section code -->
 
             <!-- Blogs Section -->
-            <section class="blogs_section">
-                <h2>BLOGS</h2>
-                <div class="blogs-container">
-                    <div class="blog-item">
-                        <img src="images/f14.png" alt="Blog Image 1">
+            <div class="container">
+                <section class="blogs_section">
+                    <h2>BLOGS</h2>
+                    <div class="blogs-container">
+                        <div class="blog-item">
+                            <img src="images/f14.png" alt="Blog Image 1">
+                        </div>
+                        <div class="blog-item">
+                            <img src="images/f15.png" alt="Blog Image 2">
+                        </div>
+                        <div class="blog-item">
+                            <img src="images/f16.png" alt="Blog Image 3">
+                        </div>
                     </div>
-                    <div class="blog-item">
-                        <img src="images/f15.png" alt="Blog Image 2">
-                    </div>
-                    <div class="blog-item">
-                        <img src="images/f16.png" alt="Blog Image 3">
-                    </div>
-                </div>
-            </section>
+                </section>
+            </div>
 
 
         </main><!-- End .main -->
