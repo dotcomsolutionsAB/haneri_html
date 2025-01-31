15d575fa3ec74e4eba6ad785a3af724780d0c2cb
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-01-31 14:19:58 +0530

    s267

--- privacy_policy.php
+++ privacy_policy.php
@@ -6,12 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Haneri</title>
+    <title>Air Curve Design</title>
     <?php include("inc_files/fav_icon_others.php"); ?>
-
     <script>
         WebFontConfig = {
-            google: { families: [ 'Open+Sans:300,400,600,700,800', 'Poppins:300,400,500,600,700' ] }
+            google: { families: [ 'Open+Sans:300,400,600,700,800', 'Poppins:300,400,500,600,700', 'Shadows+Into+Light:400', 'Segoe+Script:300,400,500,600' ] }
         };
         ( function ( d ) {
             var wf = d.createElement( 'script' ), s = d.scripts[ 0 ];
@@ -26,24 +25,85 @@
 
     <!-- Main CSS File -->
     <link rel="stylesheet" href="assets/css/demo3.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/vendor/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="assets/vendor/simple-line-icons/css/simple-line-icons.min.css">
-
-    <!-- Custom -->
-     <link rel="stylesheet" href="custom/custom.css">
-     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;700&family=Open+Sans:wght@400;700&family=Raleway:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/vendor/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="custom/responsive.css">
+    <link rel="stylesheet" href="custom/custom.css">
+    <style>
+        .page-header {
+            padding: 1rem 0 3.2rem;
+            margin-top: 0px;
+        }
+        .banner-layer h1 {
+            font-size: 80px;
+            color: #fff;
+            font-family: Poppins,sans-serif;
+        }
+        .owl-dots{
+            display:none;
+        }
+    </style>
 </head>
 
-<body class="full-screen-slider">
+<body>
     <div class="page-wrapper">
-        <header class="header">
+        <header class="header header-transparent">
             <?php include("inc_files/header.php"); ?>
         </header><!-- End .header -->
-        
-        <main class="main">
-            <?php include("inc_files/privacy_policy.php"); ?>
+
+        <main class="main about">
+            <nav aria-label="breadcrumb" class="breadcrumb-nav">
+                <div class="container">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <!-- <li class="breadcrumb-item"><a href="#">Pillar Technology</a></li> -->
+                        <li class="breadcrumb-item active" aria-current="page">Privary Policy</li>
+                    </ol>
+                </div><!-- End .container -->
+            </nav>
+            <style>
+                .about p {
+                    font-family: 'Barlow Condensed';
+                    font-size: 22px;
+                    color: #222529;
+                    text-align: justify;
+                }
+                .air_curve_design {
+                    padding: 20px 0px;
+                    padding-top: 1rem;
+                }
+                .containe h2{
+                    font-weight: 400;
+                    color: #315859;
+                }
+                .contents ul{
+                    list-style-type: auto;
+                    margin-left: 20px;
+                    font-size: 22px;
+                    /* line-height: 22px; */
+                    text-align: justify;
+                    font-family: 'Barlow Condensed';
+                    color: #222529;
+                }    
+                .contents ul li{
+                    margin-bottom: 15px;
+                }
+                strong{
+                    font-weight: 600;
+                    font-size: larger;
+                }
+            </style>
+            <!-- <div class="page-header pt-3 bg-transparent"> -->
+            <div class="heading">
+                <div class="containe text-left">
+                    <h1 class="text-uppercase text-left about_section">Air Curve Design</h1>
+                </div><!-- End .container -->
+            </div><!-- End .page-header -->
+            <div class="container">                
+                <section id="privacy_policy" class="privacy_policy">
+                    <?php include("inc_files/privacy_policy.php"); ?>
+                </section>
+            </div>
         </main><!-- End .main -->
 
         <?php include("inc_files/footer.php"); ?>
@@ -57,9 +117,8 @@
 
     <div class="mobile-menu-container">
         <!-- Mobile_sidebar code -->
-         <?php include("inc_files/mobile_sidebar.php"); ?>
-
-    </div><!-- End .mobile-menu-container -->
+        <?php include("inc_files/mobile_sidebar.php"); ?>
+    </div>
 
     <div class="sticky-navbar">
         <!-- Mobile sitky bottom nav -->
@@ -72,7 +131,7 @@
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/plugins.min.js"></script>
-    <script src="assets/js/jquery.appear.min.js"></script>
+    <script src="assets/js/jquery.plugin.min.js"></script>
 
     <!-- Main JS File -->
     <script src="assets/js/main.min.js"></script>
