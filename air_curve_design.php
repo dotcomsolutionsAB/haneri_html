f02d6d3c90ea6d21be01d24861f597d7e4961852
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-02-01 14:19:31 +0530

    s330

--- air_curve_design.php
+++ air_curve_design.php
@@ -28,6 +28,7 @@
     <link rel="stylesheet" type="text/css" href="assets/vendor/simple-line-icons/css/simple-line-icons.min.css">
     <link rel="stylesheet" type="text/css" href="assets/vendor/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="custom/responsive.css">
+    <link rel="stylesheet" href="custom/benifits_section.css">
     <link rel="stylesheet" href="custom/custom.css">
     <style>
         .page-header {
