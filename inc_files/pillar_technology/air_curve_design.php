b6096286ac1a52fff362cebc1bdd32d61565fc22
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-02-01 15:33:24 +0530

    s338

--- inc_files/pillar_technology/air_curve_design.php
+++ inc_files/pillar_technology/air_curve_design.php
@@ -149,6 +149,7 @@
         <div class="row row-bg">
             <div class="col-xl-4 col-lg-6">
             <div class="work-together-section">
+                <img src="../images/Logo1.png" alt="Work Together Image" class="fit-image" />
                 <div class="contentss">
                     <h2>The Science Behind Air Curve Design</h2>
                     <div class="c">
