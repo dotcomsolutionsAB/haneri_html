0018c647fdccaf5172fff7ca0d2535c73f4264f5
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-02-04 12:55:52 +0530

    ..

---
+++ domex.php
@@ -0,0 +1,20 @@
+<?php include("header.php"); ?>
+
+<main class="main about">
+    <nav aria-label="breadcrumb" class="breadcrumb-nav">
+        <div class="container">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <!-- <li class="breadcrumb-item"><a href="#">Pillar Technology</a></li> -->
+                <li class="breadcrumb-item active" aria-current="page">Domex</li>
+            </ol>
+        </div><!-- End .container -->
+    </nav>
+    <div class="containe text-left">
+        <h1 class="text-uppercase about_section">Domex</h1>
+    </div>
+    <div class="container"> 
+    </div>
+</main><!-- End .main -->
+
+<?php include("footer.php"); ?>
