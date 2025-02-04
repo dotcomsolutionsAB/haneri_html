506ddda62e7dc1d3d9d98b4a218c0df5d6f6cb42
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-02-04 05:47:56 +0530

    ..

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>Haneri</title>
-    <?php include("inc_files/fav_icon_others.php"); ?>
+    <?php include("../inc_files/fav_icon_others.php"); ?>
 
     <script>
         WebFontConfig = {
--- index.php
+++ index.php
@@ -1,82 +1,83 @@
-        <!-- Category section  -->
-        <main class="main">
-            <!-- Slider code -->
-            <?php include("inc_files/slider.php"); ?>
-            
-            <div class="container">
-                <!-- Featured_products code -->
-                <section class="featured_products">
-                    <h2>Featured Products</h2>
-                    <div class="product-grid">
-                        <div class="product">
-                            <a href="#"><img src="images/f1.png" alt="Black Fan"></a>
-                        </div>
-                        <div class="product">
-                            <a href="#"><img src="images/f2.png" alt="Wooden Fan"></a>
-                        </div>
-                        <div class="product">
-                            <a href="#"><img src="images/f3.png" alt="White Fan"></a>
-                        </div>
-                        <div class="product">
-                            <a href="#"><img src="images/f4.png" alt="Black Fan 2"></a>
-                        </div>
-                    </div>
-                </section>
-
-                <!-- About_haneri code -->
-                <?php include("inc_files/about_haneri.php"); ?>
-
-                <!-- Why_choose_us code -->
-                <?php include("inc_files/why_choose_us.php"); ?>
-
-                 <!-- Theme_category_section code -->
-
-            </div>
-            <!-- Best_seller_section code -->
-            <?php include("inc_files/best_seller_section.php"); ?>
-                                   
-
-            <hr class="mt-3 mb-6">
-
-            <section class="haneri-bespoke">
-                <div class="container">
-                    <div class="bespoke-content">
-                        <div class="bespoke-text">
-                            <h1>HANERI <br><span>BESPOKE</span></h1>
-                        </div>
-                        <div class="bespoke-image">
-                            <img src="images/Haneri_Website_Wireframe_V1.1.png" alt="Crafting Image">
-                        </div>
-                    </div>
+<?php include("header.php"); ?>
+<!-- Category section  -->
+<main class="main">
+    <!-- Slider code -->
+    <?php include("inc_files/slider.php"); ?>
+    
+    <div class="container">
+        <!-- Featured_products code -->
+        <section class="featured_products">
+            <h2>Featured Products</h2>
+            <div class="product-grid">
+                <div class="product">
+                    <a href="#"><img src="images/f1.png" alt="Black Fan"></a>
                 </div>
-            </section>
-
-            <hr class="mt-3 mb-6">
-            
-            <!-- Faq_question code -->
-            <?php include("inc_files/faq_question.php"); ?>
-
-            <!-- support_section code -->
-
-            <!-- Blogs Section -->
-            <div class="container">
-                <section class="blogs_section">
-                    <h2>BLOGS</h2>
-                    <div class="blogs-container">
-                        <div class="blog-item">
-                            <img src="images/f14.png" alt="Blog Image 1">
-                        </div>
-                        <div class="blog-item">
-                            <img src="images/f15.png" alt="Blog Image 2">
-                        </div>
-                        <div class="blog-item">
-                            <img src="images/f16.png" alt="Blog Image 3">
-                        </div>
-                    </div>
-                </section>
+                <div class="product">
+                    <a href="#"><img src="images/f2.png" alt="Wooden Fan"></a>
+                </div>
+                <div class="product">
+                    <a href="#"><img src="images/f3.png" alt="White Fan"></a>
+                </div>
+                <div class="product">
+                    <a href="#"><img src="images/f4.png" alt="Black Fan 2"></a>
+                </div>
             </div>
+        </section>
+
+        <!-- About_haneri code -->
+        <?php include("inc_files/about_haneri.php"); ?>
+
+        <!-- Why_choose_us code -->
+        <?php include("inc_files/why_choose_us.php"); ?>
+
+            <!-- Theme_category_section code -->
+
+    </div>
+    <!-- Best_seller_section code -->
+    <?php include("inc_files/best_seller_section.php"); ?>
+                            
+
+    <hr class="mt-3 mb-6">
+
+    <section class="haneri-bespoke">
+        <div class="container">
+            <div class="bespoke-content">
+                <div class="bespoke-text">
+                    <h1>HANERI <br><span>BESPOKE</span></h1>
+                </div>
+                <div class="bespoke-image">
+                    <img src="images/Haneri_Website_Wireframe_V1.1.png" alt="Crafting Image">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <hr class="mt-3 mb-6">
+    
+    <!-- Faq_question code -->
+    <?php include("inc_files/faq_question.php"); ?>
+
+    <!-- support_section code -->
+
+    <!-- Blogs Section -->
+    <div class="container">
+        <section class="blogs_section">
+            <h2>BLOGS</h2>
+            <div class="blogs-container">
+                <div class="blog-item">
+                    <img src="images/f14.png" alt="Blog Image 1">
+                </div>
+                <div class="blog-item">
+                    <img src="images/f15.png" alt="Blog Image 2">
+                </div>
+                <div class="blog-item">
+                    <img src="images/f16.png" alt="Blog Image 3">
+                </div>
+            </div>
+        </section>
+    </div>
 
 
-        </main><!-- End .main -->
-
+</main><!-- End .main -->
+<?php include("footer.php"); ?>
         
