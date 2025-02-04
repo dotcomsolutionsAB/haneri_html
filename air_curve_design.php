c1cbc946cfdaccd1e50bac0b2e48f2253055a751
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-02-04 06:02:21 +0530

    ..

--- air_curve_design.php
+++ air_curve_design.php
@@ -1,110 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include("header.php"); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Air Curve Design</title>
-    <?php include("inc_files/fav_icon_others.php"); ?>
-    <script>
-        WebFontConfig = {
-            google: { families: [ 'Open+Sans:300,400,600,700,800', 'Poppins:300,400,500,600,700', 'Shadows+Into+Light:400', 'Segoe+Script:300,400,500,600' ] }
-        };
-        ( function ( d ) {
-            var wf = d.createElement( 'script' ), s = d.scripts[ 0 ];
-            wf.src = 'assets/js/webfont.js';
-            wf.async = true;
-            s.parentNode.insertBefore( wf, s );
-        } )( document );
-    </script>
-
-    <!-- Plugins CSS File -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
-    <!-- Main CSS File -->
-    <link rel="stylesheet" href="assets/css/demo3.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/vendor/simple-line-icons/css/simple-line-icons.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/vendor/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="custom/responsive.css">
-    <link rel="stylesheet" href="custom/benifits_section.css">
-    <link rel="stylesheet" href="custom/custom.css">
-    <link rel="stylesheet" href="custom/custom_styles.css">
-    <style>
-        /* .page-header {
-            padding: 1rem 0 3.2rem;
-            margin-top: 0px;
-        }
-        .banner-layer h1 {
-            font-size: 80px;
-            color: #fff;
-            font-family: Poppins,sans-serif;
-        }
-        .owl-dots{
-            display:none;
-        } */
-    </style>
-</head>
-
-<body>
-    <div class="page-wrapper">
-        <header class="header header-transparent">
-            <?php include("inc_files/header.php"); ?>
-        </header><!-- End .header -->
-
-        <main class="main about">
-            <nav aria-label="breadcrumb" class="breadcrumb-nav">
-                <div class="container">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                        <!-- <li class="breadcrumb-item"><a href="#">Pillar Technology</a></li> -->
-                        <li class="breadcrumb-item active" aria-current="page">Air Curve Design</li>
-                    </ol>
-                </div><!-- End .container -->
-            </nav>
-            <!-- <div class="page-header pt-3 bg-transparent"> -->
-            <div class="heading">
-                <div class="containe text-left">
-                    <h1 class="text-uppercase text-left page_heading heading1">Air Curve Design</h1>
-                </div><!-- End .container -->
-            </div><!-- End .page-header -->
-            <div class="container">                
-                <section id="air_curve_design" class="air_curve_design">
-                    <?php include("inc_files/pillar_technology/air_curve_design.php"); ?>
-                </section>
-            </div>
-        </main><!-- End .main -->
-
-        <?php include("inc_files/footer.php"); ?>
-    </div><!-- End .page-wrapper -->
-
-    <div class="loading-overlay">
-        <?php include("inc_files/loading.php"); ?>
+<main class="main about">
+    <nav aria-label="breadcrumb" class="breadcrumb-nav">
+        <div class="container">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <!-- <li class="breadcrumb-item"><a href="#">Pillar Technology</a></li> -->
+                <li class="breadcrumb-item active" aria-current="page">Air Curve Design</li>
+            </ol>
+        </div><!-- End .container -->
+    </nav>
+    <!-- <div class="page-header pt-3 bg-transparent"> -->
+    <div class="heading">
+        <div class="containe text-left">
+            <h1 class="text-uppercase text-left page_heading heading1">Air Curve Design</h1>
+        </div><!-- End .container -->
+    </div><!-- End .page-header -->
+    <div class="container">                
+        <section id="air_curve_design" class="air_curve_design">
+            <?php include("inc_files/pillar_technology/air_curve_design.php"); ?>
+        </section>
     </div>
+</main><!-- End .main -->
 
-    <div class="mobile-menu-overlay"></div><!-- End .mobil-menu-overlay -->
-
-    <div class="mobile-menu-container">
-        <!-- Mobile_sidebar code -->
-        <?php include("inc_files/mobile_sidebar.php"); ?>
-    </div>
-
-    <div class="sticky-navbar">
-        <!-- Mobile sitky bottom nav -->
-        <?php include("inc_files/mobile_sticky_bottom_nav.php"); ?>
-    </div>
-
-    <a id="scroll-top" href="#top" title="Top" role="button"><i class="icon-angle-up"></i></a>
-
-    <!-- Plugins JS File -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/plugins.min.js"></script>
-    <script src="assets/js/jquery.plugin.min.js"></script>
-
-    <!-- Main JS File -->
-    <script src="assets/js/main.min.js"></script>
-</body>
-
-</html>
+<?php include("footer.php"); ?>
