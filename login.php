61bbc774db5561ba92eea87a194285e6e4bf29bd
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-02-13 16:47:00 +0530

    ..

--- login.php
+++ login.php
@@ -2,27 +2,12 @@
 
 		<main class="main">
 			<div class="page-header">
-				<!-- <div class="container d-flex flex-column align-items-center">
-					<nav aria-label="breadcrumb" class="breadcrumb-nav">
-						<div class="container">
-							<ol class="breadcrumb">
-								<li class="breadcrumb-item"><a href="demo4.html">Home</a></li>
-								<li class="breadcrumb-item"><a href="category.html">Shop</a></li>
-								<li class="breadcrumb-item active" aria-current="page">
-									My Account
-								</li>
-							</ol>
-						</div>
-					</nav>
-
-					<h1>My Account</h1>
-				</div> -->
 			</div>
 
 			<div class="container login-container padding_top_100">
 				<div class="row">
 					<div class="col-lg-10 mx-auto">
-						<div class="row">
+						<div class="login_container">
 							<div class="col-md-6">
 								<div class="heading mb-1">
 									<h2 class="title">Login</h2>
@@ -57,33 +42,6 @@
 									</button>
 								</form>
 							</div>
-
-							<!-- <div class="col-md-6">
-								<div class="heading mb-1">
-									<h2 class="title">Register</h2>
-								</div>
-
-								<form action="#">
-									<label for="register-email">
-										Email address
-										<span class="required">*</span>
-									</label>
-									<input type="email" class="form-input form-wide" id="register-email" required />
-
-									<label for="register-password">
-										Password
-										<span class="required">*</span>
-									</label>
-									<input type="password" class="form-input form-wide" id="register-password"
-										required />
-
-									<div class="form-footer mb-2">
-										<button type="submit" class="btn btn-dark btn-md w-100 mr-0">
-											Register
-										</button>
-									</div>
-								</form>
-							</div> -->
 						</div>
 					</div>
 				</div>
