a51f845f0baeffba3b4f06e5f3117165d3690a73
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-02-13 21:33:13 +0530

    ..

--- profile.php
+++ profile.php
@@ -6,7 +6,7 @@
 		<div class="row">
 			<div class="sidebar widget widget-dashboard mb-lg-0 mb-3 col-lg-3 order-0">
 				<h2 class="text-uppercase">My Account</h2>
-				<ul class="nav nav-tabs list flex-column mb-0" role="tablist">
+				<ul class="nav nav-tabs flex-column mb-0" role="tablist">
 					<li class="nav-item">
 						<a class="nav-link active" id="dashboard-tab" data-toggle="tab" href="#dashboard"
 							role="tab" aria-controls="dashboard" aria-selected="true">Dashboard</a>
