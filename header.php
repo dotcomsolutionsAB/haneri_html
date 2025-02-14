0dcb3f64805638d08034ee9eebb20dfa2adbe6ac
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-02-14 14:20:46 +0530

    ..

--- header.php
+++ header.php
@@ -253,13 +253,13 @@
                     <i class="icon-user-2"></i>
                 </a> |  
                 <a href="#" class="header-icon"><i class="fab fa-whatsapp"></i></a> | 
-                <a href="cart.php" class="header-icon" title="cart">
+                <a href="cart.php" class="header-icon cart" title="cart">
                     <i class="minicart-icon"></i>
                     <span class="cart-count badge-circle">3</span>
                 </a> |                
                 <div class="header-search header-search-popup header-search-category d-none d-sm-block">
-                    <a href="#" class="search-toggle" role="button"><i class="icon-magnifier"></i></a>
-                </div> |
+                    <a href="#" class="search-toggle" role="button"><i class="icon-magnifier"></i></a> |
+                </div> 
                 <a href="#" class="header-icon" id="logout-btn" title="Logout">
                     <i class="fas fa-sign-out-alt"></i>
                 </a>
