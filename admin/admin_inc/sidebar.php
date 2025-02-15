0fdbb4b790d994b9b4afa0f9287a7ec7aeb404cf
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-02-15 12:48:17 +0530

    ..

--- admin/admin_inc/sidebar.php
+++ admin/admin_inc/sidebar.php
@@ -3,26 +3,21 @@
     id="sidebar">
     <div class="sidebar-header hidden lg:flex items-center relative justify-between px-3 lg:px-6 shrink-0"
         id="sidebar_header">
-        <div class="sidebar-header hidden lg:flex items-center relative justify-between px-3 lg:px-6 shrink-0"
-            id="sidebar_header">
-            <!-- Single Logo for Light & Dark Mode -->
-            <a href="index.php">
-                <img class="default-logo min-h-[22px] max-w-none" src="uploads/Haneri_Logo.png" alt="Haneri Logo" />
-                <!-- If you want a smaller variant when sidebar is collapsed,
-                    you can still include the small-logo (just hide it unless collapsed) -->
-                <img class="small-logo min-h-[22px] max-w-none hidden" src="uploads/Haneri_Logo.png" alt="Haneri Small Logo" />
-            </a>
-
-            <!-- Sidebar Toggle Button -->
-            <button
-                class="btn btn-icon btn-icon-md size-[30px] rounded-lg border border-gray-200 dark:border-gray-300 bg-light text-gray-500 hover:text-gray-700 toggle absolute start-full top-2/4 -translate-x-2/4 -translate-y-2/4 rtl:translate-x-2/4"
-                data-toggle="body"
-                data-toggle-class="sidebar-collapse"
-                id="sidebar_toggle">
-                <i class="ki-filled ki-black-left-line toggle-active:rotate-180 transition-all duration-300 rtl:translate rtl:rotate-180 rtl:toggle-active:rotate-0"></i>
-            </button>
-</div>
-
+        <a class="dark:hidden" href="index.php">
+            <img class="default-logo min-h-[22px] max-w-none" src="uploads/Haneri_Logo.png" />
+            <img class="small-logo min-h-[22px] max-w-none" src="uploads/Haneri_Logo.png" />
+        </a>
+        <!-- <a class="hidden dark:block" href="index.php">
+            <img class="default-logo min-h-[22px] max-w-none" src="assets/media/app/default-logo-dark.svg" />
+            <img class="small-logo min-h-[22px] max-w-none" src="assets/media/app/mini-logo.svg" />
+        </a> -->
+        <button
+            class="btn btn-icon btn-icon-md size-[30px] rounded-lg border border-gray-200 dark:border-gray-300 bg-light text-gray-500 hover:text-gray-700 toggle absolute start-full top-2/4 -translate-x-2/4 -translate-y-2/4 rtl:translate-x-2/4"
+            data-toggle="body" data-toggle-class="sidebar-collapse" id="sidebar_toggle">
+            <i
+                class="ki-filled ki-black-left-line toggle-active:rotate-180 transition-all duration-300 rtl:translate rtl:rotate-180 rtl:toggle-active:rotate-0">
+            </i>
+        </button>
     </div>
     <div class="sidebar-content flex grow shrink-0 py-5 pe-2" id="sidebar_content">
         <div class="scrollable-y-hover grow shrink-0 flex ps-2 lg:ps-5 pe-1 lg:pe-3" data-scrollable="true"
