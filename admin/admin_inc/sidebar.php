63d6943628fcbd6d3bd584cfeed21f494b1da312
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-02-15 14:26:31 +0530

    ..

--- admin/admin_inc/sidebar.php
+++ admin/admin_inc/sidebar.php
@@ -122,7 +122,7 @@
                         <!-- All Orders -->
                         <div class="menu-item">
                             <a class="menu-link border border-transparent items-center grow menu-item-active:bg-secondary-active dark:menu-item-active:bg-coal-300 dark:menu-item-active:border-gray-100 menu-item-active:rounded-lg hover:bg-secondary-active dark:hover:bg-coal-300 dark:hover:border-gray-100 hover:rounded-lg gap-[14px] ps-[10px] pe-[10px] py-[8px]"
-                                href="pages/show_orders" tabindex="0">
+                                href="pages/show_orders.php" tabindex="0">
                                 <span class="menu-bullet flex w-[6px] -start-[3px] rtl:start-0 relative before:absolute before:top-0 before:size-[6px] before:rounded-full rtl:before:translate-x-1/2 before:-translate-y-1/2 menu-item-active:before:bg-primary menu-item-hover:before:bg-primary">
                                 </span>
                                 <span class="menu-title text-2sm font-normal text-gray-800 menu-item-active:text-primary menu-item-active:font-semibold menu-link-hover:!text-primary">
