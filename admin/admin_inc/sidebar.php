b0aa456ea25e0cbcda7c5bfdef33b83f04bb9e53
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-02-15 14:00:14 +0530

    ..

--- admin/admin_inc/sidebar.php
+++ admin/admin_inc/sidebar.php
@@ -182,7 +182,7 @@
                         SETTINGS
                     </span>
                 </div>
-
+                <!-- Settings Section -->
                 <div class="menu-item" data-menu-item-toggle="accordion" data-menu-item-trigger="click">
                     <div class="menu-link flex items-center grow cursor-pointer border border-transparent gap-[10px] ps-[10px] pe-[10px] py-[6px]"
                         tabindex="0">
@@ -190,9 +190,8 @@
                             <i class="ki-filled ki-security-user text-lg">
                             </i>
                         </span>
-                        <span
-                            class="menu-title text-sm font-medium text-gray-800 menu-item-active:text-primary menu-link-hover:!text-primary">
-                            Authentication
+                        <span class="menu-title text-sm font-medium text-gray-800 menu-item-active:text-primary menu-link-hover:!text-primary">
+                            Configarations
                         </span>
                         <span class="menu-arrow text-gray-400 w-[20px] shrink-0 justify-end ms-1 me-[-10px]">
                             <i class="ki-filled ki-plus text-2xs menu-item-show:hidden">
@@ -201,8 +200,19 @@
                             </i>
                         </span>
                     </div>
-                    <div
-                        class="menu-accordion gap-0.5 ps-[10px] relative before:absolute before:start-[20px] before:top-0 before:bottom-0 before:border-s before:border-gray-200">
+                    <div class="menu-accordion gap-0.5 ps-[10px] relative before:absolute before:start-[20px] before:top-0 before:bottom-0 before:border-s before:border-gray-200">
+                        <div class="menu-item">
+                            <a class="menu-link border border-transparent items-center grow menu-item-active:bg-secondary-active dark:menu-item-active:bg-coal-300 dark:menu-item-active:border-gray-100 menu-item-active:rounded-lg hover:bg-secondary-active dark:hover:bg-coal-300 dark:hover:border-gray-100 hover:rounded-lg gap-[14px] ps-[10px] pe-[10px] py-[8px]"
+                                href="pages/settings.php" tabindex="0">
+                                <span
+                                    class="menu-bullet flex w-[6px] -start-[3px] rtl:start-0 relative before:absolute before:top-0 before:size-[6px] before:rounded-full rtl:before:translate-x-1/2 before:-translate-y-1/2 menu-item-active:before:bg-primary menu-item-hover:before:bg-primary">
+                                </span>
+                                <span
+                                    class="menu-title text-2sm font-normal text-gray-800 menu-item-active:text-primary menu-item-active:font-semibold menu-link-hover:!text-primary">
+                                    Setting
+                                </span>
+                            </a>
+                        </div>
                         <div class="menu-item" data-menu-item-toggle="accordion" data-menu-item-trigger="click">
                             <div class="menu-link border border-transparent grow cursor-pointer gap-[14px] ps-[10px] pe-[10px] py-[8px]"
                                 tabindex="0">
