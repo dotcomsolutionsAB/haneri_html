8532847e0de00a14efdb5915fa5cc04f78cc6b07
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-02-19 16:55:06 +0530

    ..

--- admin/pages/show_products.php
+++ admin/pages/show_products.php
@@ -75,9 +75,9 @@
                                                     <th class="text-gray-700 font-normal min-w-[100px]">Added By</th>
                                                     <th class="text-gray-700 font-normal min-w-[100px]">Brand</th>
                                                     <th class="text-gray-700 font-normal min-w-[100px]">Category</th>
-                                                    <th class="min-w-[195px]">
+                                                    <th class="min-w-[165px]">
                                                         <span class="sort">
-                                                            <span class="sort-label text-gray-700 font-normal">Variants</span>
+                                                            <span class="sort-label text-gray-700 font-normal">Variants Info</span>
                                                             <span class="sort-icon">
                                                             </span>
                                                         </span>
@@ -803,7 +803,52 @@
                                 <td class="text-gray-800 font-normal">
                                     ${product.features && product.features.length > 0 ? product.features.map(f => `${f.feature_name}: ${f.feature_value}`).join("<br>") : "No Features Available"}
                                 </td>
-                                <td class="text-gray-800 font-normal">-</td>
+                                <td class="w-[60px]">
+                                    <div class="menu" data-menu="true">
+                                        <div class="menu-item" data-menu-item-offset="0, 10px" data-menu-item-placement="bottom-end" 
+                                            data-menu-item-placement-rtl="bottom-start" data-menu-item-toggle="dropdown" data-menu-item-trigger="click|lg:click">
+                                            
+                                            <button class="menu-toggle btn btn-sm btn-icon btn-light btn-clear">
+                                                <i class="ki-filled ki-dots-vertical"></i>
+                                            </button>
+
+                                            <div class="menu-dropdown menu-default w-full max-w-[175px]" data-menu-dismiss="true">
+                                                <div class="menu-item">
+                                                    <a class="menu-link" href="product_details.php?slug=${product.slug}">
+                                                        <span class="menu-icon"><i class="ki-filled ki-search-list"></i></span>
+                                                        <span class="menu-title">View</span>
+                                                    </a>
+                                                </div>
+                                                <div class="menu-item">
+                                                    <a class="menu-link export-product" data-product-id="${product.id}" href="#">
+                                                        <span class="menu-icon"><i class="ki-filled ki-file-up"></i></span>
+                                                        <span class="menu-title">Export</span>
+                                                    </a>
+                                                </div>
+                                                <div class="menu-separator"></div>
+                                                <div class="menu-item">
+                                                    <a class="menu-link" href="edit_product.php?slug=${product.slug}">
+                                                        <span class="menu-icon"><i class="ki-filled ki-pencil"></i></span>
+                                                        <span class="menu-title">Edit</span>
+                                                    </a>
+                                                </div>
+                                                <div class="menu-item">
+                                                    <a class="menu-link copy-product" data-product-id="${product.id}" href="#">
+                                                        <span class="menu-icon"><i class="ki-filled ki-copy"></i></span>
+                                                        <span class="menu-title">Make a copy</span>
+                                                    </a>
+                                                </div>
+                                                <div class="menu-separator"></div>
+                                                <div class="menu-item">
+                                                    <a class="menu-link remove-product" data-product-id="${product.id}" href="#">
+                                                        <span class="menu-icon"><i class="ki-filled ki-trash"></i></span>
+                                                        <span class="menu-title">Remove</span>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
                             </tr>
                         `);
                     } else {
