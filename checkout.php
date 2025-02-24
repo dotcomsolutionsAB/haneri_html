ad2763048112b9d708b7fe2a13638ed3c8bc7ccc
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-02-24 15:19:45 +0530

    ...

--- checkout.php
+++ checkout.php
@@ -91,7 +91,54 @@
         </div>
 
         <div class="row">
-            <div class="col-lg-7">
+        <div class="col-lg-7">
+            <h4>Returning customer?
+                <button data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" class="btn btn-link btn-toggle">Login</button>
+            </h4>
+
+            <div id="collapseOne" class="collapse">
+                <div class="login-section feature-box">
+                    <div class="feature-box-content">
+                        <form action="#" id="login-form">
+                            <p>
+                                If you have shopped with us before, please enter your details below. If you are a new customer, please proceed to the Billing & Shipping section.
+                            </p>
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="mb-0 pb-1">Username or email <span
+                                                class="required">*</span></label>
+                                        <input type="email" class="form-control" required />
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="mb-0 pb-1">Password <span
+                                                class="required">*</span></label>
+                                        <input type="password" class="form-control" required />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn">LOGIN</button>
+
+                            <div class="form-footer mb-1">
+                                <div class="custom-control custom-checkbox mb-0 mt-0">
+                                    <input type="checkbox" class="custom-control-input" id="lost-password" />
+                                    <label class="custom-control-label mb-0" for="lost-password">Remember
+                                        me</label>
+                                </div>
+
+                                <a href="forgot-password.html" class="forget-password">Lost your password?</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <!-- <div class="col-lg-7">
                 <ul class="checkout-steps">
                     <li>
                         <h2 class="step-title">Billing details</h2>
@@ -274,7 +321,7 @@
                         </form>
                     </li>
                 </ul>
-            </div>
+            </div> -->
             <!-- End .col-lg-8 -->
 
             <div class="col-lg-5">
