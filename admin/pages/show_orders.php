706753c0fa42b42abd08487cfc74ca3403b3a341
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-03-17 16:22:16 +0530

    ..

--- admin/pages/show_orders.php
+++ admin/pages/show_orders.php
@@ -1,5 +1,6 @@
 <base href="../">
 <?php include("../../configs/auth_check.php"); ?>
+<?php include("../../configs/config.php"); ?>
 <?php 
     $current_page = "Show Orders"; // Dynamically set this based on the page
 ?>
