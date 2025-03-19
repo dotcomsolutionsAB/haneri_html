1ad603f950efdf6a0de1d37407b7af95ecfb7af2
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-03-19 13:29:46 +0530

    ..

--- admin/pages/show_orders.php
+++ admin/pages/show_orders.php
@@ -15,11 +15,11 @@
                 <div class="container-fixed">
                     <div class="flex flex-wrap items-center lg:items-end justify-between gap-5 pb-7.5">
                         <div class="flex flex-col justify-center gap-2">
-                            <!-- <h1 class="text-xl font-medium leading-none text-gray-900" id="">
-                                Orders (00)
-                            </h1> -->
+                            <h1 class="text-xl font-medium leading-none text-gray-900" id="">
+                                Orders
+                            </h1>
                             <div class="flex items-center gap-2 text-sm font-normal text-gray-700">
-                                Overview of <span id="count-orders"> 00 </span> in Tableview.
+                                Overview of <span id="count-orders"> 00 </span>.
                             </div>
                         </div>
                         <div class="flex items-center gap-2.5">
