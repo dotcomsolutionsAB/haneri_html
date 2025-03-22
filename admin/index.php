244111836b368ee1430d68bd17ba5cb779557f52
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-03-22 14:46:38 +0530

    ..

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,9 @@
 <?php include("header.php");?>
+<?php 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+?>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 
