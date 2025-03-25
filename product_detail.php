4d1d1b8627599f3c12e35041300bc08d26e45c06
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-03-26 00:39:27 +0530

    ..

--- product_detail.php
+++ product_detail.php
@@ -60,6 +60,70 @@
                         $('#features-list').html(data.data.features.map(f => `<li>${f.feature_value}</li>`).join(''));
                         $('.about_section, .breadcrumb-title').text(data.data.name);
 
+                        // IMAGE SECTION LOGIC
+        let imageSectionHtml = '';
+
+if (data.data.id == 14) {
+    // Custom images for ID 14
+    imageSectionHtml = `
+    <div class="product-slider-container">
+        <div class="product-single-carousel owl-carousel owl-theme show-nav-hover">
+            <div class="product-item">
+                <img class="product-single-image" src="images/Natural Pine.png" data-zoom-image="images/Natural Pine.png" width="915" height="915" alt="Natural Pine" />
+            </div>
+            <div class="product-item">
+                <img class="product-single-image" src="images/Espresso Walnut.png" data-zoom-image="images/Espresso Walnut.png" width="915" height="915" alt="Espresso Walnut" />
+            </div>
+            <div class="product-item">
+                <img class="product-single-image" src="images/Moonlit White.png" data-zoom-image="images/Moonlit White.png" width="915" height="915" alt="Moonlit White" />
+            </div>
+            <div class="product-item">
+                <img class="product-single-image" src="images/Velvet Black.png" data-zoom-image="images/Velvet Black.png" width="915" height="915" alt="Velvet Black" />
+            </div>
+        </div>
+        <span class="prod-full-screen"><i class="icon-plus"></i></span>
+    </div>
+    <div class="prod-thumbnail owl-dots transparent-dots flex-column" id="carousel-custom-dots">
+        <div class="owl-dot"><img src="images/Natural Pine.png" width="98" height="98" alt="Natural Pine" /></div>
+        <div class="owl-dot"><img src="images/Espresso Walnut.png" width="98" height="98" alt="Espresso Walnut" /></div>
+        <div class="owl-dot"><img src="images/Moonlit White.png" width="98" height="98" alt="Moonlit White" /></div>
+        <div class="owl-dot"><img src="images/Velvet Black.png" width="98" height="98" alt="Velvet Black" /></div>
+    </div>`;
+} else {
+    // Default images for other products (f1.png, f2.png, f3.png)
+    imageSectionHtml = `
+    <div class="product-slider-container">
+        <div class="product-single-carousel owl-carousel owl-theme show-nav-hover">
+            <div class="product-item">
+                <img class="product-single-image" src="images/f1.png" data-zoom-image="images/f1.png" width="915" height="915" alt="product" />
+            </div>
+            <div class="product-item">
+                <img class="product-single-image" src="images/f2.png" data-zoom-image="images/f2.png" width="915" height="915" alt="product" />
+            </div>
+            <div class="product-item">
+                <img class="product-single-image" src="images/f3.png" data-zoom-image="images/f3.png" width="915" height="915" alt="product" />
+            </div>
+        </div>
+        <span class="prod-full-screen"><i class="icon-plus"></i></span>
+    </div>
+    <div class="prod-thumbnail owl-dots transparent-dots flex-column" id="carousel-custom-dots">
+        <div class="owl-dot"><img src="assets/images/products/zoom/product-1.jpg" width="98" height="98" alt="product" /></div>
+        <div class="owl-dot"><img src="assets/images/products/zoom/product-2.jpg" width="98" height="98" alt="product" /></div>
+        <div class="owl-dot"><img src="assets/images/products/zoom/product-3.jpg" width="98" height="98" alt="product" /></div>
+    </div>`;
+}
+
+// Inject the HTML
+$('#product-image-section').html(imageSectionHtml);
+
+// Re-initialize owl carousel if needed
+$('.product-single-carousel').owlCarousel({
+    items: 1,
+    nav: true,
+    dots: false,
+    loop: true
+});
+
                         // Handle variants
                         if (data.data.variants.length > 0) {
                             const variants = data.data.variants;
@@ -367,7 +431,7 @@
                                 </div> -->
                             </div>
                         </div>
-<script>
+<!-- <script>
     const data = {
         data: {
             id: 14 // Change this value to test
@@ -437,7 +501,7 @@
             </div>
         </div>`;
     }
-</script>
+</script> -->
                         <div class="col-lg-7 pb-1">
                             <div class="single-product-custom-block">
                                 <div class="porto-block">
