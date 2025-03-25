df87837dc78bf64d20b569cf9d5915b58c512763
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-03-25 15:21:19 +0530

    ..

--- index.php
+++ index.php
@@ -138,7 +138,7 @@
                         ];
                         foreach ($faqs as $faq) {
                             echo "<div class='card card-accordion'>
-                                    <a class='card-header collapsed' href='#' data-toggle='collapse' data-target='#{$faq['id']}' aria-expanded='true' aria-controls='{$faq['id']}'>
+                                    <a class='card-headers collapsed' href='#' data-toggle='collapse' data-target='#{$faq['id']}' aria-expanded='true' aria-controls='{$faq['id']}'>
                                         {$faq['question']}
                                     </a>
                                     <div id='{$faq['id']}' class='collapse' data-parent='#accordion'>
