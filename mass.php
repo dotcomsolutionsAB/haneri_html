b8ad8a0c03f04b3123e394d2097f5bf066ea79e9
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2025-03-26 15:02:38 +0530

    a20

--- mass.php
+++ mass.php
@@ -11,11 +11,11 @@
         </div><!-- End .container -->
     </nav>
     <div class="containe text-left">
-            <a class="btn button" href="/air_curve_design.php">Air Curve Design</a>
+    <a  class="btn button" href="/air_curve_design.php">Air Curve Design</a>
             <a class="btn button" href="/turbosilent_bldc.php">TurboSilent BLDC</a>
             <a class="btn button" href="/mass.php">M.A.S.S</a>
-            <a class="btn button" href="/mass.php">Lumiambience</a>
-            <a class="btn button" href="/mass.php">S.C.A.N</a>
+            <a class="btn button" href="/lumiambience.php">Lumiambience</a>
+            <a class="btn button" href="/scan.php">S.C.A.N</a>
     </div>
     <div class="container">                        
         <section id="mass" class="mass">
--- turbosilent_bldc.php
+++ turbosilent_bldc.php
@@ -12,11 +12,11 @@
     <!-- <div class="page-header pt-3 bg-transparent"> -->
     <div class="heading">
         <div class="containe text-left">
-            <a class="btn button" href="/air_curve_design.php">Air Curve Design</a>
+        <a  class="btn button" href="/air_curve_design.php">Air Curve Design</a>
             <a class="btn button" href="/turbosilent_bldc.php">TurboSilent BLDC</a>
             <a class="btn button" href="/mass.php">M.A.S.S</a>
-            <a class="btn button" href="/mass.php">Lumiambience</a>
-            <a class="btn button" href="/mass.php">S.C.A.N</a>
+            <a class="btn button" href="/lumiambience.php">Lumiambience</a>
+            <a class="btn button" href="/scan.php">S.C.A.N</a>
         </div><!-- End .container -->
     </div><!-- End .page-header -->
 
