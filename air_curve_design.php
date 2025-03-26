e6354f62fb602c3cf36aa4ba66489c8ec97dd296
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2025-03-26 14:23:40 +0530

    a5

--- air_curve_design.php
+++ air_curve_design.php
@@ -13,8 +13,8 @@
     <!-- <div class="page-header pt-3 bg-transparent"> -->
     <div class="heading">
         <div class="containe text-left">
-            <button style="padding-top: 5px;" class="btn button" href="https://haneri.ongoingsites.xyz/air_curve_design.php">Air Curve Design</button>
-            <button style="padding-top: 5px;" class="btn button" href="https://haneri.ongoingsites.xyz/turbosilent_bldc.php">TurboSilent BLDC</button>
+            <button style="padding-top: 5px;" class="btn button" href="/air_curve_design.php">Air Curve Design</button>
+            <button style="padding-top: 5px;" class="btn button" href="/turbosilent_bldc.php">TurboSilent BLDC</button>
         </div><!-- End .container -->
     </div><!-- End .page-header -->
     <div class="container">                
