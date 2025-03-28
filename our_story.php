88e922af79a388b229dc7e0da72f328454a5396d
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2025-03-28 16:05:14 +0530

    a73

--- our_story.php
+++ our_story.php
@@ -11,11 +11,13 @@
     </nav>
     <!-- <div class="page-header pt-3 bg-transparent"> -->
     <div class="heading">
-        <div class="button-group">
-        <a  class="btn button" href="/our_story.php">Our Story</a>
-            <a class="btn button" href="/our_brands.php">Our Brands</a>
-            <a class="btn button" href="/capabilities.php">Capabilities</a>
-        </div><!-- End .container -->
+    <div class="button-group container" style="display: flex; gap: 10px; align-items: center;">
+    <a class="btn button" href="/our_story.php">Our Story</a>
+    <span class="divider">|</span>
+    <a class="btn button" href="/our_brands.php">Our Brands</a>
+    <span class="divider">|</span>
+    <a class="btn button" href="/capabilities.php">Capabilities</a>
+    </div><!-- End .container -->
     </div><!-- End .page-header -->
     <div class="container">       
         <section id="our-story" class="our_story">
