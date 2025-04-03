2c7dece7fe7cc88f011d306a5ab4123a0045edbd
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-04-03 13:25:48 +0530

    ..

--- inc_files/featured_products.php
+++ inc_files/featured_products.php
@@ -1,4 +1,6 @@
 <?php include("configs/config.php"); ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
 
 <section class="featured">
     <h2 class="heading_1">Featured Products</h2>
@@ -8,6 +10,8 @@
     </div>
 </section>
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 
 
 <script>
