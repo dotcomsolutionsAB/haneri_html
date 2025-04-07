2497b76c866f2e86776b6b705d19ab18ccc15c81
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-04-07 15:17:39 +0530

    ..

--- privacy_policy.php
+++ privacy_policy.php
@@ -11,11 +11,6 @@
         </div><!-- End .container -->
     </nav>
 
-    <div class="heading">
-        <div class="containe text-left">
-            <h1 class="text-uppercase text-left about_section">Privacy Policy</h1>
-        </div><!-- End .container -->
-    </div><!-- End .page-header -->
     <div class="container">                
         <div id="privacy_policy" class="privacy_policy">
             <?php include("inc_files/policy/privacy_policy.php"); ?>
