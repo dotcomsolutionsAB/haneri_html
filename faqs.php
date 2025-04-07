a0960048bfbe0a139e9c7ee0ff4c88425cedde04
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-04-07 13:59:57 +0530

    ..

--- faqs.php
+++ faqs.php
@@ -5,17 +5,18 @@
         <div class="container">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                <li class="breadcrumb-item"><a href="#">Pillar Technology</a></li>
+                <!-- <li class="breadcrumb-item"><a href="#">Pillar Technology</a></li> -->
                 <li class="breadcrumb-item active" aria-current="page">FaQs</li>
             </ol>
         </div>
     </nav>
 
-    <div class="heading">
+    <!-- <div class="heading">
         <div class="containe text-left">
             <h1 class="text-uppercase text-left about_section">FAQs</h1>
-        </div><!-- End .container -->
-    </div><!-- End .page-header -->
+        </div>
+    </div> -->
+
     <div class="container">
         <?php include("inc_files/policy/faq.php"); ?>
     </div>
--- our_brands.php
+++ our_brands.php
@@ -10,11 +10,11 @@
         </div><!-- End .container -->
     </nav>
     <div class="button-group container" style="display: flex; gap: 10px; align-items: center;">
-    <a class="btn button" href="/our_story.php">Our Story</a>
-    <span class="divider">|</span>
-    <a class="btn button" href="/our_brands.php">Our Brands</a>
-    <span class="divider">|</span>
-    <a class="btn button" href="/capabilities.php">Capabilities</a>
+        <a class="btn button" href="/our_story.php">Our Story</a>
+        <span class="divider">|</span>
+        <a class="btn button" href="/our_brands.php">Our Brands</a>
+        <span class="divider">|</span>
+        <a class="btn button" href="/capabilities.php">Capabilities</a>
     </div>
     <div class="container">    
         <section id="our-brands" class="brands-section">
