d3bb15709dd3fe3e0c19eaabcf54daebbf80c67a
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-04-09 17:21:03 +0530

    ..

--- header.php
+++ header.php
@@ -312,7 +312,7 @@
 
     /* Show the dropdown menu when hovering over Profile link */
     .header-icon-wrapper:hover .dropdown-menu {
-        display: block;
+        display: block !important;
     }
 
     /* Optional: style for icon hover */
@@ -363,7 +363,7 @@
         //         `;
         //     }
         // });
-        
+
 document.addEventListener("DOMContentLoaded", function() {
     const authToken = localStorage.getItem("auth_token");
 
