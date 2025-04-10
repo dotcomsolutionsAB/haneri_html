f938adac6d297362e8f5ec2ed9be446dd6a3f0b7
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-04-10 12:36:18 +0530

    ..

--- sample_header.php
+++ sample_header.php
@@ -252,9 +252,9 @@
                                     <a href="#" class="dropdown-item" id="logout-link">Logout</a>
                                 </div> 
                                 | 
-                                <div class="header-search header-search-popup header-search-category d-none d-sm-block">
+                                <!-- <div class="header-search header-search-popup header-search-category d-none d-sm-block">
                                     <a href="#" class="search-toggle" role="button"><i class="icon-magnifier"></i></a>
-                                </div>
+                                </div> -->
                             </div>
 
                         <?php else : ?>
@@ -267,25 +267,24 @@
                                 <!-- <div class="header-search header-search-popup header-search-category d-none d-sm-block">
                                     <a href="#" class="search-toggle" role="button"><i class="icon-magnifier"></i></a>
                                 </div> -->
-                                <div class="relative">
-                                    <div class="header-search hidden sm:block">
-                                        <button id="searchToggle" class="text-gray-700 hover:text-yellow-500 transition">
-                                            <i class="icon-magnifier text-xl"></i>
-                                        </button>
-
-                                        <div id="searchBar" class="absolute right-0 top-12 bg-yellow-300 p-3 rounded-lg shadow-lg w-72 hidden z-50 animate-slide-in">
-                                            <div class="relative">
-                                                <input type="text" id="searchInput" placeholder="Search..." 
-                                                    class="w-full px-4 py-2 pr-10 rounded-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-yellow-500 shadow-inner">
-                                                <span id="clearSearch" 
-                                                    class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 text-xl cursor-pointer hidden">&times;</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
                             </div>
                         <?php endif; ?>
+                        <div class="relative">
+                            <div class="header-search hidden sm:block">
+                                <button id="searchToggle" class="text-gray-700 hover:text-yellow-500 transition">
+                                    <i class="icon-magnifier text-xl"></i>
+                                </button>
+
+                                <div id="searchBar" class="absolute right-0 top-12 bg-yellow-300 p-3 rounded-lg shadow-lg w-72 hidden z-50 animate-slide-in">
+                                    <div class="relative">
+                                        <input type="text" id="searchInput" placeholder="Search..." 
+                                            class="w-full px-4 py-2 pr-10 rounded-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-yellow-500 shadow-inner">
+                                        <span id="clearSearch" 
+                                            class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 text-xl cursor-pointer hidden">&times;</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
@@ -421,23 +420,6 @@
                             </a>
                         </div>
                     </div> |
-                    <div class="relative">
-                        <div class="header-search hidden sm:block">
-                            <button id="searchToggle" class="text-gray-700 hover:text-yellow-500 transition">
-                                <i class="icon-magnifier text-xl"></i>
-                            </button>
-
-                            <div id="searchBar" class="absolute right-0 top-12 bg-yellow-300 p-3 rounded-lg shadow-lg w-72 hidden z-50 animate-slide-in">
-                                <div class="relative">
-                                    <input type="text" id="searchInput" placeholder="Search..." 
-                                        class="w-full px-4 py-2 pr-10 rounded-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-yellow-500 shadow-inner">
-                                    <span id="clearSearch" 
-                                        class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 text-xl cursor-pointer hidden">&times;</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
             `;
 
@@ -457,9 +439,6 @@
                         <i class="icon-user-2"></i>
                     </a> |  
                     <a href="#" class="header-icon"><i class="fab fa-whatsapp"></i></a> |
-                    <div class="header-search header-search-popup header-search-category d-none d-sm-block">
-                        <a href="#" class="search-toggle" role="button"><i class="icon-magnifier"></i></a>
-                    </div>
                 </div>
             `;
         }
