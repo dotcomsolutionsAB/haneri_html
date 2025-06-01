4dfc3d65460e2da0baeb6ae3275b8afc03d0e483
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2025-06-01 11:58:55 +0530

    a5

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
                 style="background-color: #ecc;" alt="home banner">
             <div class="banner-layer appear-animate" data-animation-name="fadeInUpShorter">
                 <h1><strong>A true</strong><br><span> revolution</span></h1>
-                <p class="slider_text">Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</p>
+                <p class="slider_text">Haneri is the brainchild of a passionate team with over 75 years of collective <br>
+                experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</p>
                     <a href="#" class="btn btn-dark slider_btn btn-xl" role="button">Shop Now</a>
             </div><!-- End .banner-layer -->
         </div><!-- End .home-slide -->
