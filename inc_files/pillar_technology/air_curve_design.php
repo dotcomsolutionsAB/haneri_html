7c2664d4b036d490124b4305582206e2b9f0c0c4
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2025-06-12 22:23:59 +0530

    a42

--- inc_files/pillar_technology/air_curve_design.php
+++ inc_files/pillar_technology/air_curve_design.php
@@ -7,10 +7,10 @@
             </div>
             <!-- Text on the right side -->
             <div class="col-md-6">
-               <div style="width: 894px; height: 182px">
+               <div class="col-md-6" style="width: 894px; height: 182px">
                 <span style="color: #00473E; font-size: 50px; font-family: Barlow Condensed; font-weight: 500; line-height: 100px; word-wrap: break-word">Introducing Air Curve Design: </span><span style="color: #00473E; font-size: 50px; font-family: Barlow Condensed; font-weight: 300; line-height: 100px; word-wrap: break-word">Redefining Ceiling Fan Efficiency<br/></span></div>
             </div>
-            <div style="width: 876px; color: black; font-size: 16px; font-family: Open Sans; font-weight: 400; line-height: 28.80px; word-wrap: break-word">Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</div>
+            <div class="col-md-6" style="width: 876px; color: black; font-size: 16px; font-family: Open Sans; font-weight: 400; line-height: 28.80px; word-wrap: break-word">Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</div>
         </div>
     </section>
 </body>
