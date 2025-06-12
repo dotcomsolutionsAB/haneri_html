4664f398b91dff247aa55a9d467f0dea1ea567ad
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2025-06-12 22:28:43 +0530

    a44

--- inc_files/pillar_technology/air_curve_design.php
+++ inc_files/pillar_technology/air_curve_design.php
@@ -1,18 +1,4 @@
 <body>
-<section class="content-container">
-        <div class="row align-items-center">
-            <!-- Image on the left side -->
-            <div class="col-md-6">
-                <img src="https://placehold.co/600x400" alt="Air Curve Design" class="img-fluid">
-            </div>
-            <!-- Text on the right side -->
-            <div class="col-md-6">
-               <div style="width: 894px; height: 182px">
-                <span style="color: #00473E; font-size: 50px; font-family: Barlow Condensed; font-weight: 500; line-height: 100px; word-wrap: break-word">Introducing Air Curve Design: </span><span style="color: #00473E; font-size: 50px; font-family: Barlow Condensed; font-weight: 300; line-height: 100px; word-wrap: break-word">Redefining Ceiling Fan Efficiency<br/></span></div>
-            </div>
-            <div style="width: 876px; color: black; font-size: 16px; font-family: Open Sans; font-weight: 400; line-height: 28.80px; word-wrap: break-word">Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</div>
-        </div>
-    </section>
     <section class="container my-5">
         <div class="row align-items-center">
             <!-- Image on the left side -->
@@ -21,7 +7,7 @@
             </div>
             <!-- Text on the right side -->
             <div class="col-md-6">
-                <h2>What is Air Curve Design?</h2>
+                <div style="width: 894px; height: 182px"><span style="color: #00473E; font-size: 80px; font-family: Barlow Condensed; font-weight: 500; line-height: 100px; word-wrap: break-word">Introducing Air Curve Design: </span><span style="color: #00473E; font-size: 80px; font-family: Barlow Condensed; font-weight: 300; line-height: 100px; word-wrap: break-word">Redefining Ceiling Fan Efficiency<br/></span></div>
                 <p>Air Curve Design is a cutting-edge blade technology that leverages advanced computational modelling and simulation to craft blades optimized for maximum airflow and energy efficiency. This innovation ensures that every curve and angle of the blade is scientifically tailored for superior aerodynamics, delivering a ceiling fan that offers unparalleled performance, energy efficiency, and style.</p>
             </div>
         </div>
