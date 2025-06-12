027d47950be5a115cef5efeb1461c0e506dd5f73
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2025-06-12 22:46:36 +0530

    a49

--- inc_files/pillar_technology/air_curve_design.php
+++ inc_files/pillar_technology/air_curve_design.php
@@ -15,17 +15,17 @@
     </section>
     <section class="container my-5">
         <div class="row align-items-center">
-            <!-- Image on the left side -->
-            <div class="col-md-6">
-                <img src="https://placehold.co/600x400" alt="Air Curve Design" class="img-fluid">
-            </div>
             <!-- Text on the right side -->
-            <div class="col-md-6">
+             <div class="col-md-6">
                 <h2><span style="color: #00473E; font-size: 50px; font-family: Barlow Condensed; font-weight: 500;">Introducing Air Curve Design: </span><span style="color: #00473E; font-size: 50px; font-family: Barlow Condensed; font-weight: 300;">Redefining Ceiling Fan Efficiency<br/></span></h2>
                 <p>Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</p>
                 <p>Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</p>
             </div>
         </div>
+            <!-- Image on the left side -->
+           <div class="col-md-6">
+                <img src="https://placehold.co/600x400" alt="Air Curve Design" class="img-fluid">
+            </div>
     </section>
 </body>
 </html>
