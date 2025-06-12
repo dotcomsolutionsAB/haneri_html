5b5e9971eddc111e63acc2a9035a3ff228595514
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2025-06-12 22:30:59 +0530

    a45

--- inc_files/pillar_technology/air_curve_design.php
+++ inc_files/pillar_technology/air_curve_design.php
@@ -7,7 +7,7 @@
             </div>
             <!-- Text on the right side -->
             <div class="col-md-6">
-                <div style="width: 894px; height: 182px"><span style="color: #00473E; font-size: 80px; font-family: Barlow Condensed; font-weight: 500; line-height: 100px; word-wrap: break-word">Introducing Air Curve Design: </span><span style="color: #00473E; font-size: 80px; font-family: Barlow Condensed; font-weight: 300; line-height: 100px; word-wrap: break-word">Redefining Ceiling Fan Efficiency<br/></span></div>
+                <h2 style="width: 894px; height: 182px"><span style="color: #00473E; font-size: 50px; font-family: Barlow Condensed; font-weight: 500; line-height: 100px;">Introducing Air Curve Design: </span><span style="color: #00473E; font-size: 50px; font-family: Barlow Condensed; font-weight: 300; line-height: 100px;">Redefining Ceiling Fan Efficiency<br/></span></h2>
                 <p>Air Curve Design is a cutting-edge blade technology that leverages advanced computational modelling and simulation to craft blades optimized for maximum airflow and energy efficiency. This innovation ensures that every curve and angle of the blade is scientifically tailored for superior aerodynamics, delivering a ceiling fan that offers unparalleled performance, energy efficiency, and style.</p>
             </div>
         </div>
