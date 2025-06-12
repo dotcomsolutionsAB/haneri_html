6db5b65a797facd964a52bd6744ae8781cb6af1a
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2025-06-12 22:17:34 +0530

    a39

--- inc_files/pillar_technology/air_curve_design.php
+++ inc_files/pillar_technology/air_curve_design.php
@@ -7,7 +7,7 @@
             </div>
             <!-- Text on the right side -->
             <div class="col-md-6">
-               <div style="width: 894px; height: 182px"><span style="color: #00473E; font-size: 80px; font-family: Barlow Condensed; font-weight: 500; line-height: 100px; word-wrap: break-word">Introducing Air Curve Design: </span><span style="color: #00473E; font-size: 80px; font-family: Barlow Condensed; font-weight: 300; line-height: 100px; word-wrap: break-word">Redefining Ceiling Fan Efficiency<br/></span></div>
+               <div style="width: 894px; height: 182px"><span style="color: #00473E; font-size: 50px; font-family: Barlow Condensed; font-weight: 500; line-height: 100px; word-wrap: break-word">Introducing Air Curve Design: </span><span style="color: #00473E; font-size: 50px; font-family: Barlow Condensed; font-weight: 300; line-height: 100px; word-wrap: break-word">Redefining Ceiling Fan Efficiency<br/></span></div>
             </div>
         </div>
     </section>
