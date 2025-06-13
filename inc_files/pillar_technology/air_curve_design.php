6c1a470147cd66160c76d1b39b940a46c2f3610d
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2025-06-13 14:09:26 +0530

    a60

--- inc_files/pillar_technology/air_curve_design.php
+++ inc_files/pillar_technology/air_curve_design.php
@@ -31,6 +31,26 @@
             </div>
         </div>
     </section>
+     <section class="container my-5">
+        <div style="justify-content: center; display: flex; flex-direction: column; color: #00473E; font-size: 80px; font-family: Barlow Condensed; font-weight: 400; word-wrap: break-word">Key Features of Air Curve Design</div>
+    </section>
+        <section class="container my-5">
+        <div class="row align-items-center">
+            <!-- Image on the left side -->
+            <div class="col-md-6">
+                <img src="../assets/images/3.png" alt="Air Curve Design" class="img-fluid">
+            </div>
+            <!-- Text on the right side -->
+            <div class="col-md-6">
+               <div><span style="color: #00473E; font-size: 40px; font-family: Barlow Condensed; font-weight: 500; word-wrap: break-word">The Science Behind Air Curve Design
+                <div style="color: black; font-size: 14px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">Computational Fluid Dynamics (CFD) Advanced CFD simulations analyze airflow patterns, ensuring that each blade design achieves peak aerodynamic efficiency.
+                                                                                                                            Prototyping and Testing Multiple prototypes are tested under real-world conditions to validate performance metrics such as air delivery, noise levels, and energy consumption.
+                                                                                                                            Iterative Design Continuous refinement based on data-driven insights ensures that AirCurve blades exceed industry benchmarks for performance and reliability.</div>
+                <br>
+                <div style="color: black; font-size: 14px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</div>
+            </div>
+        </div>
+    </section>
 
 </body>
 </html>
