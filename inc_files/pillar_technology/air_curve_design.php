77b651191d46075b58ca01d7d2a6170ffe09a0f9
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-06-17 00:51:30 +0530

    ..

--- inc_files/pillar_technology/air_curve_design.php
+++ inc_files/pillar_technology/air_curve_design.php
@@ -7,10 +7,13 @@
             </div>
             <!-- Text on the right side -->
             <div class="col-md-6">
-               <div><span style="color: #00473E; font-size: 40px; font-family: Barlow Condensed; font-weight: 500; word-wrap: break-word">Introducing Air Curve Design: </span><br><span style="color: #00473E; font-size: 40px; font-family: Barlow Condensed; font-weight: 300; word-wrap: break-word">Redefining Ceiling Fan Efficiency<br/></span></div>
-                <div style="color: black; font-size: 14px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</div>
+               <div>
+                    <span class="air_curve_span_h2">Introducing Air Curve Design: </span>
+                    <br>
+                    <span class="air_curve_span_h2">Redefining Ceiling Fan Efficiency<br/></span></div>
+                <div class="air_curve_span">Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</div>
                 <br>
-                <div style="color: black; font-size: 14px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</div>
+                <div class="air_curve_span">Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</div>
             </div>
         </div>
     </section>
@@ -18,12 +21,18 @@
         <div class="row align-items-center">
             <!-- Text on the left side -->
             <div class="col-md-6 order-md-1 order-2">
-                <h2><span style="color: #00473E; font-size: 50px; font-family: Barlow Condensed; font-weight: 500;">What is Air Curve Design?</h2>
-                <div><span style="color: black; font-size: 14px; font-family: Open Sans; font-weight: 600; word-wrap: break-word">Air Curve Design</span><span style="color: black; font-size: 16px; font-family: Open Sans; font-weight: 400; word-wrap: break-word"> is a cutting-edge blade technology that leverages advanced computational modelling and simulation to craft blades optimized for maximum airflow and energy efficiency. This innovation ensures that every curve and angle of the blade is scientifically tailored for superior aerodynamics, delivering a ceiling fan that offers unparalleled performance, energy efficiency, and style.</span></div>
+                <h2><span class="air_curve_span_h2">What is Air Curve Design?</h2>
+                <div>
+                    <span class="air_curve_span">Air Curve Design</span>
+                    <span class="air_curve_span2"> is a cutting-edge blade technology that leverages advanced computational modelling and simulation to craft blades optimized for maximum airflow and energy efficiency. This innovation ensures that every curve and angle of the blade is scientifically tailored for superior aerodynamics, delivering a ceiling fan that offers unparalleled performance, energy efficiency, and style.</span></div>
                 <br>
-                <div><span style="color: black; font-size: 14px; font-family: Open Sans; font-weight: 600; word-wrap: break-word">Air Curve Design</span><span style="color: black; font-size: 16px; font-family: Open Sans; font-weight: 400; word-wrap: break-word"> is a cutting-edge blade technology that leverages advanced computational modelling and simulation to craft blades optimized for maximum airflow and energy efficiency. This innovation ensures that every curve and angle of the blade is scientifically tailored for superior aerodynamics, delivering a ceiling fan that offers unparalleled performance, energy efficiency, and style.</span></div>
+                <div>
+                    <span class="air_curve_span">Air Curve Design</span>
+                    <span class="air_curve_span2"> is a cutting-edge blade technology that leverages advanced computational modelling and simulation to craft blades optimized for maximum airflow and energy efficiency. This innovation ensures that every curve and angle of the blade is scientifically tailored for superior aerodynamics, delivering a ceiling fan that offers unparalleled performance, energy efficiency, and style.</span></div>
                 <br>
-                <div><span style="color: black; font-size: 14px; font-family: Open Sans; font-weight: 600; word-wrap: break-word">Air Curve Design</span><span style="color: black; font-size: 16px; font-family: Open Sans; font-weight: 400; word-wrap: break-word"> is a cutting-edge blade technology that leverages advanced computational modelling and simulation to craft blades optimized for maximum airflow and energy efficiency. This innovation ensures that every curve and angle of the blade is scientifically tailored for superior aerodynamics, delivering a ceiling fan that offers unparalleled performance, energy efficiency, and style.</span></div>
+                <div>
+                    <span class="air_curve_span">Air Curve Design</span>
+                    <span class="air_curve_span2"> is a cutting-edge blade technology that leverages advanced computational modelling and simulation to craft blades optimized for maximum airflow and energy efficiency. This innovation ensures that every curve and angle of the blade is scientifically tailored for superior aerodynamics, delivering a ceiling fan that offers unparalleled performance, energy efficiency, and style.</span></div>
             </div>
             <!-- Image on the right side -->
             <div class="col-md-6 order-md-2 order-1">
@@ -31,9 +40,28 @@
             </div>
         </div>
     </section>
-    <!-- <section class="container my-5">
-        <div style="justify-content: center; display: flex; flex-direction: column; color: #00473E; font-size: 40px; font-family: Barlow Condensed; font-weight: 400; word-wrap: break-word">Key Features of Air Curve Design</div>
-    </section> -->
+    <style>
+        .air_curve_span{
+            color: black; 
+            font-size: 14px; 
+            font-family: Open Sans; 
+            font-weight: 600; 
+            word-wrap: break-word;
+        }
+        .air_curve_span2{
+            color: black; 
+            font-size: 16px; 
+            font-family: Open Sans; 
+            font-weight: 400; 
+            word-wrap: break-word
+        }
+        .air_curve_span_h2{
+            color: #00473E; 
+            font-size: 50px; 
+            font-family: Barlow Condensed; 
+            font-weight: 500;
+        }
+    </style>
 
     <section class="air_curve-grid-features container my-5">
     <h2 class="air_curve-title">Key Features of Air Curve Design</h2>
@@ -95,12 +123,10 @@
             </div>
             <!-- Text on the right side -->
             <div class="col-md-6">
-               <div><span style="color: #00473E; font-size: 40px; font-family: Barlow Condensed; font-weight: 500; word-wrap: break-word">The Science Behind Air Curve Design
-                <div style="color: black; font-size: 14px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">Computational Fluid Dynamics (CFD) Advanced CFD simulations analyze airflow patterns, ensuring that each blade design achieves peak aerodynamic efficiency.
-                                                                                                                            Prototyping and Testing Multiple prototypes are tested under real-world conditions to validate performance metrics such as air delivery, noise levels, and energy consumption.
-                                                                                                                            Iterative Design Continuous refinement based on data-driven insights ensures that AirCurve blades exceed industry benchmarks for performance and reliability.</div>
+               <div><span class="air_curve_span_h2">The Science Behind Air Curve Design
+                <div class="air_curve_span">Computational Fluid Dynamics (CFD) Advanced CFD simulations analyze airflow patterns, ensuring that each blade design achieves peak aerodynamic efficiency.Prototyping and Testing Multiple prototypes are tested under real-world conditions to validate performance metrics such as air delivery, noise levels, and energy consumption. Iterative Design Continuous refinement based on data-driven insights ensures that AirCurve blades exceed industry benchmarks for performance and reliability.</div>
                 <br>
-                <div style="color: black; font-size: 14px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</div>
+                <div class="air_curve_span">Haneri is the brainchild of a passionate team with over 75 years of collective experience in the consumer durable industry. With expertise spanning product creation, innovation, engineering, and manufacturing, we envisioned Haneri as a brand that caters to consumers seeking products that seamlessly blend with modern living. At Haneri, our mission is to inspire everyday life by offering thoughtfully designed, functional, and future-ready solutions.</div>
             </div>
         </div>
     </section>
