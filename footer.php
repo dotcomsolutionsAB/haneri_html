57ea42918559fc4439f60e08ddcc813c2d5f8fb6
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-06-16 13:55:02 +0530

    ..

--- footer.php
+++ footer.php
@@ -12,33 +12,9 @@
         <div class="container">
             <div class="footer-top top-border d-flex align-items-center justify-content-between flex-wrap">
                 <div class="footer-left widget-newsletter d-md-flex align-items-center">
-                    <!-- <div class="widget-newsletter-info">
-                        <h5 class="widget-newsletter-title text-uppercase m-b-1">subscribe newsletter</h5>
-                        <p class="widget-newsletter-content mb-0">Get all the latest information on Events, Sales
-                            and Offers.</p>
-                    </div>
-                    <form action="#">
-                        <div class="footer-submit-wrapper d-flex">
-                            <input type="email" class="form-control" placeholder="Email address..." size="40"
-                                required>
-                            <button type="submit" class="btn btn-dark btn-sm">Subscribe</button>
-                        </div>
-                    </form> -->
-
-                    <!-- <div class="footer_logo">
-                        <a href="#">
-                            <img src="images/logo_white.png" alt="logo">
-                        </a>
-                    </div> -->
+                    
 
                 </div>
-                <!-- <div class="footer-right">
-                    <div class="social-icons">
-                        <a href="#" class="social-icon social-facebook icon-facebook" target="_blank"></a>
-                        <a href="#" class="social-icon social-twitter icon-twitter" target="_blank"></a>
-                        <a href="#" class="social-icon social-instagram icon-instagram" target="_blank"></a>
-                    </div>
-                </div> -->
             </div>
 
             <div class="footer-middle">
@@ -64,10 +40,6 @@
                                     <li><a href="lumiambience.php">Lumiambience</a></li>
                                     <li><a href="scan.php">S.C.A.N</a></li>
                                 </ul>
-                                <!-- <ul class="link-part col-xl-6">
-                                    <li class="pl-xl-2 ml-xl-1"><a href="#">Orders History</a></li>
-                                    <li class="pl-xl-2 ml-xl-1"><a href="#">Advanced Search</a></li>
-                                </ul> -->
                             </div>
                         </div><!-- End .widget -->
                     </div>
@@ -81,10 +53,6 @@
                                     <li><a href="shipping_policy.php">Shipping Policy</a></li>
                                     <li><a href="wir_policy.php">WIR Policy</a></li>
                                 </ul>
-                                <!-- <ul class="link-part col-xl-6">
-                                    <li class="pl-xl-2 ml-xl-1"><a href="#">Orders History</a></li>
-                                    <li class="pl-xl-2 ml-xl-1"><a href="#">Advanced Search</a></li>
-                                </ul> -->
                             </div>
                         </div><!-- End .widget -->
                     </div>
@@ -96,7 +64,6 @@
                             <div class="row">
                                 <div class="col-sm-12">
                                     <div class="contact-widget">
-                                        <!-- <h4 class="widget-title">ADDRESS:</h4> -->
                                         <p><i class="fas fa-home"></i></p>
                                         <p>
                                             <span>
@@ -160,23 +127,12 @@
             <div class="footer-bottom d-sm-flex align-items-center">
                 <div class="footer-left">
                     <span class="footer-copyright">&copy; 2025 | Haneri </span>
-                    <!-- <span class="footer-copyright">&copy; 2025 Dotcom Solutions. All rights reserved.</span> -->
                 </div>
                 <div class="footer-right ml-auto mt-1 mt-sm-0">
-                    <div class="social-icons">
+                    <!-- <div class="social-icons">
                         <a href="#" class="social-icon social-facebook icon-facebook" target="_blank"></a>
                         <a href="#" class="social-icon social-twitter icon-twitter" target="_blank"></a>
                         <a href="#" class="social-icon social-instagram icon-instagram" target="_blank"></a>
-                    </div>
-                    <!-- <div class="payment-icons mr-0">
-                        <span class="payment-icon visa"
-                            style="background-image: url(assets/images/payments/payment-visa.svg)"></span>
-                        <span class="payment-icon paypal"
-                            style="background-image: url(assets/images/payments/payment-paypal.svg)"></span>
-                        <span class="payment-icon stripe"
-                            style="background-image: url(assets/images/payments/payment-stripe.png)"></span>
-                        <span class="payment-icon verisign"
-                            style="background-image:  url(assets/images/payments/payment-verisign.svg)"></span>
                     </div> -->
                 </div>
             </div>
@@ -201,7 +157,7 @@
         <?php include("../inc_files/mobile_sticky_bottom_nav.php"); ?>
     </div>
 
-    <a id="scroll-top" href="#top" title="Top" role="button"><i class="icon-angle-up"></i></a>
+    <!-- <a id="scroll-top" href="#top" title="Top" role="button"><i class="icon-angle-up"></i></a> -->
 
     <!-- Plugins JS File -->
     <script src="assets/js/jquery.min.js"></script>
