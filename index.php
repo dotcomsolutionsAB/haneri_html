28acfe7be26538101ac441f13ccc06a7171cbaf0
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-06-18 18:39:02 +0530

    ..

--- index.php
+++ index.php
@@ -3,20 +3,22 @@
 <!-- Category section  -->
 <main class="main">
     <!-- Slider section -->
-    <div class="home-slider desktop-slider slide-animate owl-carousel owl-theme show-nav-hover nav-big">
+    <div class="home-slider slide-animate owl-carousel owl-theme show-nav-hover nav-big">
         <div class="home-slide home-slide1 banner d-flex align-items-center">
             <img class="slide-bg" src="images/Slider1_old.jpg"
                 style="background-color: #ecc;" alt="home banner">
-        </div>
-        <div class="home-slide home-slide1 banner d-flex align-items-center">
-            <img class="slide-bg" src="images/Slider1_old.jpg"
-                style="background-color: #ecc;" alt="home banner">
-        </div>
-        <div class="home-slide home-slide1 banner d-flex align-items-center">
-            <img class="slide-bg" src="images/Slider1_old.jpg"
-                style="background-color: #ecc;" alt="home banner">
-        </div>
-    </div>
+            <div class="banner-layer appear-animate" data-animation-name="fadeInUpShorter">
+                <h1><strong>A true</strong><br><span> revolution</span></h1>
+                <p class="slider_text">Haneri is the brainchild of a passionate team with over 75 years of collective <br>
+                experience in the consumer durable industry. With expertise spanning <br>
+                product creation, innovation, engineering, and manufacturing, we envisioned <br>
+                Haneri as a brand that caters to consumers seeking products that seamlessly <br>
+                blend with modern living. At Haneri, our mission is to inspire everyday life by <br>
+                offering thoughtfully designed, functional, and future-ready solutions.</p>
+                    <a href="#" class="btn btn-dark slider_btn btn-xl" role="button">Buy Now</a>
+            </div><!-- End .banner-layer -->
+        </div><!-- End .home-slide -->
+    </div><!-- End .home-slider -->
 
     
 
