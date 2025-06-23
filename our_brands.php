cd355c52860433babc3b1e6e8c1df3f5747cdff9
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-06-23 14:05:55 +0530

    ..

--- our_brands.php
+++ our_brands.php
@@ -2,13 +2,14 @@
 
 <main class="main about">
     <nav aria-label="breadcrumb" class="breadcrumb-nav">
-        <div class="container">
+        <!-- <div class="container">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="index.php">Home</a></li>
                 <li class="breadcrumb-item active" aria-current="page">Our Brands</li>
             </ol>
-        </div><!-- End .container -->
+        </div> -->
     </nav>
+    
     <?php include("our_story_links.php"); ?>
     <div class="container">    
         <section id="our-brands" class="brands-section">
