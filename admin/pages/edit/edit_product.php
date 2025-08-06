f1a804bdbba73d679ff3d2e03633bb2bfc767b2f
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-08-06 13:39:37 +0530

    ..

--- admin/pages/edit/edit_product.php
+++ admin/pages/edit/edit_product.php
@@ -1,6 +1,6 @@
-<base href="../../">
-<?php include("../../../configs/auth_check.php"); ?>
-<?php include("../../../configs/config.php"); ?>
+<base href="../">
+<?php include("../../configs/auth_check.php"); ?>
+<?php include("../../configs/config.php"); ?>
 <?php 
     $current_page = "Edit Product"; // Dynamically set this based on the page
 ?>
@@ -286,4 +286,4 @@
 </style>
 
             <!-- Footer -->
-<?php include("../footer1.php"); ?>
+<?php include("footer1.php"); ?>
