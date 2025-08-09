81c8847218335d6d1c1dd30033c029a0fce03cdf
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-08-09 15:52:43 +0530

    ..

--- contact.php
+++ contact.php
@@ -39,7 +39,7 @@
 		<!-- Map Section -->
 		<div class="contact_114">
 			<iframe 
-				src="https://maps.app.goo.gl/sPHFT8g94AyXJTWE7?g_st=iw" 
+				src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3502.9191005485295!2d77.35124647549972!3d28.602203675681356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjjCsDM2JzA3LjkiTiA3N8KwMjEnMTMuOCJF!5e0!3m2!1sen!2sin!4v1754734913098!5m2!1sen!2sin" 
 				width="100%" 
 				height="100%" 
 				style="border:0;" 
