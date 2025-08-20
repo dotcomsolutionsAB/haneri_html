75680d2cf74e1b54409e8c561b7fe91767be4acc
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-08-20 18:05:38 +0530

    ..

--- shop.php
+++ shop.php
@@ -185,7 +185,7 @@
     .filter_label{
         display: flex;
         align-items: center;
-        justify-content: space-between;
+        /* justify-content: space-between; */
         gap: 10px;
     }
     /* Style the default checkbox */
