d93464e44cc561d5bdf6769f4c89adf530ddc29a
Shaqlin <shaqlinmondal8905@gmail.com>
2025-09-15 16:13:38 +0530

    ..

--- inc_files/video_section.php
+++ inc_files/video_section.php
@@ -5,7 +5,7 @@
     <!-- Video 1 -->
     <div class="video-card">
       <iframe width="100%" height="100%" 
-        src="https://www.youtube.com/embed/h1NYXQkYO8Q?autoplay=1&mute=1&loop=1&playlist=h1NYXQkYO8Q&controls=0&modestbranding=1&rel=0" 
+        src="https://www.youtube.com/embed/h1NYXQkYO8Q?autoplay=1&mute=1&loop=1&playlist=h1NYXQkYO8Q&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3" 
         title="Video 1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
       </iframe>
     </div>
@@ -13,7 +13,7 @@
     <!-- Video 2 -->
     <div class="video-card">
       <iframe width="100%" height="100%" 
-        src="https://www.youtube.com/embed/Ahkt6Wrg1GI?autoplay=1&mute=1&loop=1&playlist=Ahkt6Wrg1GI&controls=0&modestbranding=1&rel=0" 
+        src="https://www.youtube.com/embed/Ahkt6Wrg1GI?autoplay=1&mute=1&loop=1&playlist=Ahkt6Wrg1GI&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3" 
         title="Video 2" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
       </iframe>
     </div>
@@ -21,7 +21,7 @@
     <!-- Video 3 -->
     <div class="video-card">
       <iframe width="100%" height="100%" 
-        src="https://www.youtube.com/embed/KrCYPR5_Vt8?autoplay=1&mute=1&loop=1&playlist=KrCYPR5_Vt8&controls=0&modestbranding=1&rel=0" 
+        src="https://www.youtube.com/embed/KrCYPR5_Vt8?autoplay=1&mute=1&loop=1&playlist=KrCYPR5_Vt8&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3" 
         title="Video 3" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
       </iframe>
     </div>
